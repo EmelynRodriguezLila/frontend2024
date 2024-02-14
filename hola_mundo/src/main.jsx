c74563be67caf7c2618450cec39c5babef1f09c4
Emelyn Rodriguez Lila <emelynlila04li@gmail.com>
2024-02-14 07:53:57 -0600

    Mis trabajos

---
+++ hola_mundo/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+/*ESTA LINEA IMPORTA EL CSS (LA HOJA DE ESTILOS DE LA PAGINA
+import './index.css'*/
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ inserta_datos/src/App.jsx
@@ -0,0 +1,44 @@
+const persona={
+nombre: "Emelyn Rodriguez Lila",
+edad: 21,
+profesión: "Estudiante",
+pasatiempos: [
+  "Jugar",
+  "Dormir",
+  "Noviar"
+],
+hobbie: "Mi Tilino",
+signo: "Escorpio",
+fechaN: "01/11/02",
+lugarN: "Tuxtepec"
+}
+
+function App() {
+
+  return (
+    <div>
+      <h1>
+        Información Personal
+      </h1>
+      <hr />
+      <ul>
+        <li>Nombre: {persona.nombre}</li>
+        <li>Edad: {persona.edad}</li>
+        <li>Profesión: {persona.profesión}</li>
+        <li>Pasatiempos:</li>
+          <ul>
+            <li>{persona.pasatiempos[0]}</li>
+            <li>{persona.pasatiempos[1]}</li>
+            <li>{persona.pasatiempos[2]}</li>
+          </ul>
+        <li>Hobbies: {persona.hobbie}</li>
+        <li>Fecha de naciemiento: {persona.fechaN}</li>
+        <li>Lugar de naciemiento: {persona.lugarN}</li>
+        <li>Signo Zodiacal:{persona.signo}</li>
+      
+      </ul>
+    </div>
+  )
+}
+
+export default App
